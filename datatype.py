83e2ab0204eec71072a018eb2199f0567adc41ab
krinssanghani <mrkrinsh@gmail.com>
2026-01-03 11:30:32 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,40 @@
+"""x = ("hello world")
+print (x)"""
+
+"""x = 209
+print (x)"""
+
+"""x=20.599
+print(x)"""
+
+"""x=1j
+print(x)"""
+
+"""x=["Gauva","Graps","Watermalon"]
+print(x)"""
+
+"""x=("Gauva","Graps","Watermalon")
+print(x)"""
+
+"""x=range(90)
+print(x)"""
+
+"""x={"Name","JAimin","age":18}
+print(x)"""
+
+x={"Gauva","Graps","Watermalon"}
+print(x)
+
+"""x=frozenset({"Gauva","Graps","Watermalon"})
+print(x)"""
+
+"""x=True
+print(x)"""
+
+"""x=
+print(x)"""
+
+"""x=b"Hello"
+print(x)"""
+
+
