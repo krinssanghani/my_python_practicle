1b7280ae337e9c01b38a1cfdd40f66cffb2cbc2b
krinssanghani <sanghani2484@gmail.com>
2026-01-28 11:37:14 +0530

    Add files via upload

---
+++ operator_1.py
@@ -0,0 +1,50 @@
+a=0
+print(a)
+
+#1. Assignment operator
+x=10
+print(x)
+
+#2. unary minus
+a=5
+print(-a)
+
+#3. Relational operator
+p=10
+q=5
+print(p>q)
+print(p==q)
+
+#4. Logical operator
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+
+#5.Boolean
+is_pass=True
+print(is_pass)
+
+#6. Bitwise operators
+a=5
+b=3
+print(a & b)
+print(a|b)
+
+#7. Membership Operator
+numbers=[1,2,3,4]
+print(2 in numbers)
+print(5 not in numbers)
+
+#8. Identity operators
+x=10
+y=10
+print(x is y)
+print(x is not y)
+
+#9. Arithmetic Operator
+a=10
+b=3
+print(a+b)
+print(a%b)
