4b983bb0ebae9345176fa737ce2a8965e54499e4
krinssanghani <sanghani2484@gmail.com>
2026-01-31 11:33:15 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,15 @@
+#IF....ELSE loop
+age=int(input("Enter your Age : "))
+if age>=18:
+    print("yes you are eligible for vote")
+else:
+    print("Sorry you are not eligible for vote")
+    
+#IF...ELIF LOOP
+marks=int(input("Enter your marks : "))
+if marks>=90:
+    print("you will get grade a")
+elif marks>=60:
+    print("You will get grade b")
+else:
+    print("better luck next time")
