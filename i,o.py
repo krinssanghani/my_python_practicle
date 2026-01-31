b0efdb0d48c96c22184bed16a7d9e5dddd915264
krinssanghani <sanghani2484@gmail.com>
2026-01-31 10:24:06 +0530

    Add files via upload

---
+++ i,o.py
@@ -0,0 +1,9 @@
+#OUTPUT STATEMENT
+print("Hello world")
+print("Hello Atmiya")
+
+#INPUT STATEMENT
+name = input("Enter Your Name :")
+print("hello",name)
+age = input("Enter Your age :")
+print("Your age is : ",age)
