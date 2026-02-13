91630d9500281ccbe4a55b2057a270830f4ac5f2
krinssanghani <sanghani2484@gmail.com>
2026-02-13 12:52:33 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,43 @@
+#1. print number from 1 to 5
+for i in range(1,6):
+    print(i)
+
+#2. print a message for 4 time
+for i in range(4):
+    print("hellow")
+
+#3.print n number from 1 to 10
+for i in range(1,11):
+    print(i)
+
+#4.print even number 1 to 20
+for i in range(1,21):
+    if i%2 == 0
+    print(i)
+
+#5.print odd number from 1to 15
+for i in range(1,16):
+    if i%2 !=0
+    print(i)
+
+
+#6.print table of 5
+for i in range (1,11):
+    print("5 x",i,"=",5*1)
+
+#7.print characters of a string
+name="atmiya"
+for i in name:
+    print(i)
+
+#8.sum of number of 1 to 5
+total=0
+for i in range(1,6):
+    total=total+1
+    print("sum is:",total)
+
+
+#9.print list elements
+numbers=[10,20,30,40]
+for n in numbers:
+    print(n)
