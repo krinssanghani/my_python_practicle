4038ce8c8425c7a61bb0291afe5713e4df041a74
krinssanghani <sanghani2484@gmail.com>
2026-04-04 11:11:47 +0530

    Add files via upload

---
+++ merge.py
@@ -0,0 +1,13 @@
+#merge two file into a third file
+f1=open("one.txt","r")
+data1=f1.read()
+f1.close()
+f2=open("two.txt","r")
+data2=f2.read()
+f2.close()
+
+merged=open("merged.txt","w")
+merged.write(data1)
+merged.write("\n")
+merged.write(data2)
+merged.close()
