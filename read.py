0381852edf70808b053c044ba9c74ee76d312d83
krinssanghani <sanghani2484@gmail.com>
2026-04-04 11:13:24 +0530

    Add files via upload

---
+++ read.py
@@ -0,0 +1,34 @@
+#1. read() method
+f=open("one.txt","r")
+data=f.read()
+print("file content: " ,data)
+f.close()
+
+#2. reading specific number of characters from a file using read()
+f=open("one.txt","r")
+data=f.read(10)
+print("file part : ",data)
+f.close()
+
+#3. readline()-read one line
+f=open("one.txt","r")
+line1=f.readline()
+line2=f.readline()
+line3=f.readline()
+print("line 1:",line1)
+print("line 2:",line2)
+print("line 3:",line3)
+f.close()
+
+#4. Reading specific line from a file using readlines()
+f=open("one.txt","r")
+lines=f.readlines()
+print(lines[1].strip())
+f.close()
+
+#5. readlines() - Read all lines into a list
+f=open("one.txt","r")
+lines=f.readlines()
+print("list of lines :",lines)
+print("number of lines:",len(lines))
+f.close()
